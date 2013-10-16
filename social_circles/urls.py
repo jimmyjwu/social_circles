a7709e1cf51dc730e943c58ae29403e0adaeed46
Jimmy Wu <jimmywu126@gmail.com>
2013-10-17 01:32:38 +0900

    Converted spaces to tabs.

--- social_circles/urls.py
+++ social_circles/urls.py
@@ -5,15 +5,15 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'social_circles.views.home', name='home'),
-    # url(r'^social_circles/', include('social_circles.foo.urls')),
+	# Examples:
+	# url(r'^$', 'social_circles.views.home', name='home'),
+	# url(r'^social_circles/', include('social_circles.foo.urls')),
 
-    # Maps '/admin/doc' to admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Maps '/admin/doc' to admin documentation:
+	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Maps '/admin/' to admin panel
-    url(r'^admin/', include(admin.site.urls)),
+	# Maps '/admin/' to admin panel
+	url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^', include('friends.urls')),
+	url(r'^', include('friends.urls')),
 )
