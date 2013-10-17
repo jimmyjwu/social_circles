ae8d95974ff318f730af76a54d4b0b260bfa96cd
Jimmy Wu <jimmywu126@gmail.com>
2013-10-17 22:20:30 +0900

    Code cleanup.

--- friends/constants.py
+++ friends/constants.py
@@ -2,6 +2,9 @@
 constants.py contains constant values used throughout the 'friends' app.
 """
 
+# Graph API Options
+REQUEST_TIMEOUT_IN_SECONDS = 15
+
 # FQL Queries
 FRIENDS_ID_AND_NAME_QUERY = 'SELECT uid, name FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())'
 FRIENDSHIPS_BETWEEN_FRIENDS_QUERY = 'SELECT uid1, uid2 FROM friend WHERE uid1 IN (SELECT uid2 FROM friend WHERE uid1=me()) AND uid2 IN (SELECT uid2 FROM friend WHERE uid1=me())'
