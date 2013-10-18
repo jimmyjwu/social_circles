0509ab287217ef981de2446304b0a2e729ed7f67
Jimmy Wu <jimmywu126@gmail.com>
2013-10-18 22:25:55 +0900

    Restored code from before un-merge.

--- friends/constants.py
+++ friends/constants.py
@@ -4,7 +4,7 @@ constants.py contains constant values used throughout the 'friends' app.
 
 # Graph API Options
 REQUEST_TIMEOUT_IN_SECONDS = 15
-DEVELOPER_ACCESS_TOKEN = 'CAACEdEose0cBAMBlwqbUQl1DU8LwwDcuA5qEzoSVS8gbPZCP5c4aoOkjj1x5NEfSNkdiHvWtKumvtb1EbZC89BjHA6pliE6xLLCjPj9HC2wsRAihzhoUi534bOtekzrsmdIPXMzW6GwtOdGHRc3qRqk0NFp00mmnXmjIVEHZBMYLozEU5ZCZAH2OCHmLpVugZD'
+DEVELOPER_ACCESS_TOKEN = 'CAACEdEose0cBAJAnIUlLH6kX01j0qdlcZCOSLDT3qRlsUBDzxq5bt5gVsR9ZAgxHaZBxBA8gCCYM50gMNKVGe7OsseKeax8lZBQv3jxCyXSNcft5Fb7OgvtJ7ZA9YLzLCUuElnjKhg4qQxZADc2RinnIivJpaQiqBt5ZCkGlnaU0KqBT6jp0vOLlWREYcRkrJkZD'
 
 # FQL Queries
 FRIENDS_ID_AND_NAME_QUERY = 'SELECT uid, name FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())'
--- friends/views.py
+++ friends/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 import time
 import facebook
 import networkx
+import networkx.algorithms as algorithms
+import networkx.algorithms.approximation as approximation
 
 # Local modules
 from utilities import *
@@ -15,14 +17,28 @@ access_token = DEVELOPER_ACCESS_TOKEN
 
 
 def index(request):
+	# Create Facebook Graph API object
 	facebook_graph = facebook.GraphAPI(access_token=access_token, timeout=REQUEST_TIMEOUT_IN_SECONDS)
-	profile = facebook_graph.get_object('me')
 
+	# Query for local region (friends and friendships among them)
+	me = facebook_graph.get_object('me')
 	friends = facebook_graph.fql(FRIENDS_ID_AND_NAME_QUERY)
 	friendships_between_friends = facebook_graph.fql(FRIENDSHIPS_BETWEEN_FRIENDS_QUERY)
 
+	# Construct graph of local region
+	friends_graph = networkx.Graph()
+	for friend in friends:
+		friends_graph.add_edge(me['id'], friend['uid'])
+	for friendship in friendships_between_friends:
+		friends_graph.add_edge(friendship['uid1'], friendship['uid2'])
+
+	print "me['id'] is a " + str(type(me['id']))
+	print "friend['uid'] is a " + str(type(friends[0]['uid']))
+	print "friendship['uid1'] is a " + str(type(friendships_between_friends[0]['uid1']))
+	print "friendship['uid2'] is a " + str(type(friendships_between_friends[0]['uid2']))
+
 	# Render page with friend information
-	return render(request, 'friends/index.html', {'friends': friends, 'number_of_connections_between_friends': len(friendships_between_friends)})
+	return render(request, 'friends/index.html', {'friends': friends, 'number_of_friendships': len(friends_graph.edges())})
 
 
 
