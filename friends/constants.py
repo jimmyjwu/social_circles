0d71a436b435ea041a44b5eefb8760e88ce7f396
Jimmy Wu <jimmywu126@gmail.com>
2013-10-19 18:32:28 +0900

    Added parametrized query for friendships between user's friends and a given subset of user's friends.

--- friends/constants.py
+++ friends/constants.py
@@ -4,8 +4,9 @@ constants.py contains constant values used throughout the 'friends' app.
 
 # Graph API Options
 REQUEST_TIMEOUT_IN_SECONDS = 15
-DEVELOPER_ACCESS_TOKEN = 'CAACEdEose0cBAJAnIUlLH6kX01j0qdlcZCOSLDT3qRlsUBDzxq5bt5gVsR9ZAgxHaZBxBA8gCCYM50gMNKVGe7OsseKeax8lZBQv3jxCyXSNcft5Fb7OgvtJ7ZA9YLzLCUuElnjKhg4qQxZADc2RinnIivJpaQiqBt5ZCkGlnaU0KqBT6jp0vOLlWREYcRkrJkZD'
+DEVELOPER_ACCESS_TOKEN = 'CAACEdEose0cBAPscLnx3mPS7g9TSO18ho3d5Yjl3WBM1FQyj0dXGcbj69Ok2zQcAhYN4Cv2PqZA0d3PvMvlGlHmzL4oututX6AAzG7gdYJ4G0N7tIQHz3puwVMhw3dvo6jy7mwZBVQC4Oz83lwKEN3f1ZC8RwAsIZBMCMDXwTYWi8FJkIMf4GJmNZC82RfswZD'
 
 # FQL Queries
 FRIENDS_ID_AND_NAME_QUERY = 'SELECT uid, name FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())'
 FRIENDSHIPS_BETWEEN_FRIENDS_QUERY = 'SELECT uid1, uid2 FROM friend WHERE uid1 IN (SELECT uid2 FROM friend WHERE uid1=me()) AND uid2 IN (SELECT uid2 FROM friend WHERE uid1=me())'
+FRIENDSHIPS_BETWEEN_FRIENDS_AND_PEOPLE_QUERY = lambda people: 'SELECT uid1, uid2 FROM friend WHERE uid1 IN (' + ','.join(people) + ') AND uid2 in (SELECT uid2 FROM friend WHERE uid1=me())'
