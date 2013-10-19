bf9bcc34d2d5d97a40f97920a42a12ba5b9bb106
Jimmy Wu <jimmywu126@gmail.com>
2013-10-19 23:05:03 +0900

    Added debug block to verify uniqueness of edges.

--- friends/views.py
+++ friends/views.py
@@ -26,6 +26,9 @@ def index(request):
 	friend_IDs = [unicode(friend['uid']) for friend in friends]
 
 	# Query, in chunks, for friendships among friends
+	# TO-DO: determine experimentally what chunk size to use
+	# (find one such that the number of friendships (u,v) returned is safely fewer than 5,000)
+	# TO-DO: use FQL multi-query (switch to facepy library)
 	friends_partition = list(chunks(friend_IDs, NUMBER_OF_FRIENDS_PER_FRIENDSHIP_REQUEST))
 	friendships_between_friends = []
 	for friends_sublist in friends_partition:
@@ -38,7 +41,6 @@ def index(request):
 		friends_graph.add_edge(me['id'], friend_ID)
 	for friendship in friendships_between_friends:
 		friends_graph.add_edge(friendship['uid1'], friendship['uid2'])
-		print(friendship['uid1'] + ', ' + friendship['uid2'])
 
 	"""
 	# DEBUG
@@ -46,6 +48,18 @@ def index(request):
 	print "friendship['uid1'] is a " + str(type(friendships_between_friends[0]['uid1']))
 	print "friendship['uid2'] is a " + str(type(friendships_between_friends[0]['uid2']))
 	"""
+
+	"""
+	# DEBUG
+	# Result: found that 10,197 edges are indeed unique; if (u,v) exists, (v,u) doesn't.
+	edges = friends_graph.edges()
+	edge_set = set(edges)
+	for u, v in edges:
+		if (v, u) in edge_set:
+			print('DUPLICATE DETECTED')
+	print(edge_set)
+	"""
+
 
 	# Render page with friend information
 	return render(request, 'friends/index.html', {'friends': friends, 'number_of_friendships': len(friends_graph.edges())})
