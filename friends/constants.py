9f6cebafa52af6e23311f247d6506ad4c3e2c1c6
Jimmy Wu <jimmywu126@gmail.com>
2013-10-20 00:35:54 +0900

    Improved naming conventions.

--- friends/constants.py
+++ friends/constants.py
@@ -10,5 +10,5 @@ DEVELOPER_ACCESS_TOKEN = 'CAACEdEose0cBABHqoiBAUZBSZBwnP1UqYbWhbmmbndyAir8IH4N1p
 FRIENDS_ID_AND_NAME_QUERY = 'SELECT uid, name FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())'
 FRIENDSHIPS_BETWEEN_FRIENDS_QUERY = 'SELECT uid1, uid2 FROM friend WHERE uid1 IN (SELECT uid2 FROM friend WHERE uid1=me()) AND uid2 IN (SELECT uid2 FROM friend WHERE uid1=me())'
 
-FRIENDSHIPS_BETWEEN_FRIENDS_AND_PEOPLE_QUERY = lambda people: 'SELECT uid1, uid2 FROM friend WHERE uid1 IN (' + ','.join(people) + ') AND uid2 in (SELECT uid2 FROM friend WHERE uid1=me())'
+FRIENDSHIPS_BETWEEN_FRIENDS_AND_PEOPLE_QUERY = lambda friend_IDs: 'SELECT uid1, uid2 FROM friend WHERE uid1 IN (' + ','.join(friend_IDs) + ') AND uid2 in (SELECT uid2 FROM friend WHERE uid1=me())'
 NUMBER_OF_FRIENDS_PER_FRIENDSHIP_REQUEST = 60
