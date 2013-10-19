39759ef70a218abe8aeb3d574cb48b406bfb6fc8
Jimmy Wu <jimmywu126@gmail.com>
2013-10-20 06:54:38 +0900

    Implemented FQL multiquery for user ('me') and friends; ~0.5 seconds saved. Refactored multiquery results parsing to utilities file.

--- friends/constants.py
+++ friends/constants.py
@@ -6,6 +6,7 @@ constants.py contains constant values used throughout the 'friends' app.
 DEVELOPER_ACCESS_TOKEN = 'CAACEdEose0cBALNCbK6rfRNW5MRrLM9zrWMqu0FXT5HR7gZAZAcAGlOjhHR6xtpX4W7XOArFEjboVrSZAWs1Dibxt6WdO09SY1JetNF6lzgreggXrGHfLUnWHtoMtGLWXNxYJUyXstOBOxuWk1HiZAZA8GntfkF8mZBq3yOvZCu3JKZCfX5SOTggV3qZCXB9D9M4ZD'
 
 # FQL Queries
+USER_ID_AND_NAME_QUERY = 'SELECT uid, name FROM user WHERE uid=me()'
 FRIENDS_ID_AND_NAME_QUERY = 'SELECT uid, name FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())'
 FRIENDSHIPS_BETWEEN_FRIENDS_QUERY = 'SELECT uid1, uid2 FROM friend WHERE uid1 IN (SELECT uid2 FROM friend WHERE uid1=me()) AND uid2 IN (SELECT uid2 FROM friend WHERE uid1=me())'
 
--- friends/views.py
+++ friends/views.py
@@ -22,8 +22,19 @@ def index(request):
 
 	# Query for user and friends
 	start = time.time()
-	me = facebook_graph.get('me')
-	friends = facebook_graph.fql(FRIENDS_ID_AND_NAME_QUERY)['data']
+
+	#user = facebook_graph.get('me')
+	#friends = facebook_graph.fql(FRIENDS_ID_AND_NAME_QUERY)['data']
+
+	user_and_friends_multiquery = {
+		'user': USER_ID_AND_NAME_QUERY,
+		'friends': FRIENDS_ID_AND_NAME_QUERY,
+	}
+	user_and_friends_multiquery_results = facebook_graph.fql(user_and_friends_multiquery)
+	user_and_friends_results_dictionary = parse_multiquery_results(user_and_friends_multiquery_results)
+	user = user_and_friends_results_dictionary['user'][0]
+	friends = user_and_friends_results_dictionary['friends']
+
 	friend_IDs = [unicode(friend['uid']) for friend in friends]
 	end = time.time()
 	print_execution_time('getting user and friends', start, end)
@@ -33,15 +44,15 @@ def index(request):
 	friend_ID_strings = [str(ID) for ID in friend_IDs]	# Friend UIDs must be strings (not unicode) to use in FQL multiqueries
 	friends_sublists = chunks(friend_ID_strings, NUMBER_OF_FRIENDS_PER_FRIENDSHIP_REQUEST)
 	friendships_multiquery = {'query_' + str(sublist_index): FRIENDSHIPS_BETWEEN_FRIENDS_AND_PEOPLE_QUERY(friends_sublists[sublist_index]) for sublist_index in range(len(friends_sublists))}
-	friendship_multiquery_results = facebook_graph.fql(friendships_multiquery)['data']
-	friendships_between_friends = parse_multiquery_results(friendship_multiquery_results)
+	friendships_multiquery_results = facebook_graph.fql(friendships_multiquery)
+	friendships_between_friends = parse_and_combine_multiquery_results(friendships_multiquery_results)
 	end = time.time()
 	print_execution_time('getting friendships between friends', start, end)
 
 	# Construct graph of local region
 	start = time.time()
 	friends_graph = networkx.Graph()
-	[friends_graph.add_edge(me['id'], friend_ID) for friend_ID in friend_IDs]
+	[friends_graph.add_edge(user['uid'], friend_ID) for friend_ID in friend_IDs]
 	[friends_graph.add_edge(friendship['uid1'], friendship['uid2']) for friendship in friendships_between_friends]
 	end = time.time()
 	print_execution_time('constructing graph of local region', start, end)
