bfd467ac904d5b38a6c496c6e330ea7b0091be2a
Jimmy Wu <jimmywu126@gmail.com>
2013-10-19 18:51:11 +0900

    Added generator function to split a list into chunks.

--- friends/utilities.py
+++ friends/utilities.py
@@ -7,3 +7,10 @@ def print_execution_time(operation, start_time, end_time):
 	Outputs the execution time (in seconds) of an operation to the console.
 	"""
 	print(operation + ' took ' + str('%.1f' % (end_time - start_time)) + ' seconds.')
+
+def chunks(full_list, chunk_size):
+    """
+    Generator function; yields successive sublists of specified size from a list.
+    """
+    for index in xrange(0, len(full_list), chunk_size):
+        yield full_list[index:index+chunk_size]
