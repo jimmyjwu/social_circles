d526859bafce5d16093ed06978a1d2c15a45d1af
Jimmy Wu <jimmywu126@gmail.com>
2013-10-24 23:10:39 +0900

    Changed a naming convention.

--- friends/constants.py
+++ friends/constants.py
@@ -14,4 +14,4 @@ FRIENDS_ID_AND_NAME_QUERY = 'SELECT uid, name FROM user WHERE uid IN (SELECT uid
 
 MUTUAL_FRIEND_COUNTS_QUERY = 'SELECT mutual_friend_count, uid FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1=me())'
 FRIENDSHIPS_BETWEEN_FRIENDS_AND_PEOPLE_QUERY = lambda friend_IDs: 'SELECT uid1, uid2 FROM friend WHERE uid1 IN (' + ','.join(friend_IDs) + ') AND uid2 in (SELECT uid2 FROM friend WHERE uid1=me())'
-NUMBER_OF_FRIENDS_PER_FRIENDSHIP_QUERY = lambda number_of_friends, graph_density=DEFAULT_GRAPH_DENSITY: int(SAFE_RESULTS_PER_QUERY / graph_density / number_of_friends)
+FRIEND_COUNT_PER_FRIENDSHIP_QUERY = lambda number_of_friends, graph_density=DEFAULT_GRAPH_DENSITY: int(SAFE_RESULTS_PER_QUERY / graph_density / number_of_friends)
