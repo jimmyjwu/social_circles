8d3bbc3284a82e955b73a1ce393d95597754d60e
Jimmy Wu <jimmywu126@gmail.com>
2013-10-25 00:44:51 +0900

    Improved comments.

--- friends/views.py
+++ friends/views.py
@@ -20,7 +20,7 @@ def index(request):
 	# Create Facebook Graph API object
 	facebook_graph = facepy.GraphAPI(oauth_token=access_token)
 
-	# Query for user and friends
+	# Get user and friends
 	start = time.time()
 	user_and_friends_multiquery = {
 		'user': USER_ID_AND_NAME_QUERY,
@@ -33,7 +33,7 @@ def index(request):
 	end = time.time()
 	print_execution_time('getting user and friends', start, end)
 
-	# User FQL to calculate graph density
+	# Use FQL to get mutual friend counts and calculate graph density
 	start = time.time()
 	mutual_friend_counts_results = facebook_graph.fql(MUTUAL_FRIEND_COUNTS_QUERY)['data']
 	mutual_friend_counts = [result['mutual_friend_count'] for result in mutual_friend_counts_results]
@@ -42,7 +42,7 @@ def index(request):
 	end = time.time()
 	print_execution_time('calculating graph density', start, end)
 
-	# Use FQL multi-query to get friendships among friends
+	# Use FQL multiquery to get friendships among friends
 	start = time.time()
 	friend_UID_strings = [str(friend['uid']) for friend in friends]	# Friend UIDs must be strings (not unicode) to use in FQL multiqueries
 	friend_chunks = shuffled_chunks(friend_UID_strings, FRIEND_COUNT_PER_FRIENDSHIP_QUERY(len(friends)))
@@ -53,7 +53,7 @@ def index(request):
 	end = time.time()
 	print_execution_time('getting local friendships', start, end)
 
-	# Construct graph of local region
+	# Build graph of local region
 	start = time.time()
 	friends_graph = networkx.Graph()
 	[friends_graph.add_edge(user['uid'], friend['uid']) for friend in friends]
